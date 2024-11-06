a0b51d5219000954e5167ad4d94f2f523b89957d
Diederik0007 <diederikroos@hotmail.com>
2024-11-06 17:46:52 +0100

    updateScore js

--- script.js
+++ script.js
@@ -4,3 +4,35 @@ let currentRoundNumber = 1;
 
 // Write your code below:
 
+function generateTarget() {
+    return targetNumber =  (Math.floor(Math.random()*10));
+  }
+
+function compareGuesses() {
+const targetNumber = generateTarget();
+const humanDiff = Math.abs(4-targetNumber);
+const compDiff = Math.abs(8-targetNumber);
+if (humanDiff <= compDiff) {
+        return true;
+    return true;
+} else {
+    return false;
+}
+}
+
+function updateScore() {
+    if (compareGuesses() === true) {
+      humanScore = humanScore+1;
+      return 'human';
+  
+      console.log(humanScore);
+      console.log(computerScore);
+      console.log(currentRoundNumber);
+    } else {
+      computerScore = computerScore+1;
+        return 'computer';
+      console.log(humanScore);
+      console.log(computerScore);
+      console.log(currentRoundNumber);
+    }
+  }
