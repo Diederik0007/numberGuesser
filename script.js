88ed6516c67fc5058eff7f79f670e01920c0b970
Diederik0007 <diederikroos@hotmail.com>
2024-11-07 21:38:55 +0100

    function compareGuess and updateRound

--- script.js
+++ script.js
@@ -3,21 +3,37 @@ let computerScore = 0;
 let currentRoundNumber = 1;
 
 // Write your code below:
-console.log(humanScore);
-console.log(computerScore);
-console.log(currentRoundNumber);
-
 function generateTarget() {
-  return targetNumber =  (Math.floor(Math.random()*10));
+  return (targetNumber = Math.floor(Math.random() * 10));
 }
 
-function compareGuesses(userGuess, computerGuess, targetNumber) {
-  const userDifference = Math.abs(userGuess - targetNumber);
+let winner = "";
+
+function compareGuesses(humanGuess, computerGuess, targetNumber) {
+  const userDifference = Math.abs(humanGuess - targetNumber);
   const computerDifference = Math.abs(computerGuess - targetNumber);
 
   if (userDifference <= computerDifference) {
+    winner = "human";
     return true; // Human wins or it's a tie
   } else {
+    winner = "computer"
     return false; // Computer wins
   }
 }
+
+function updateScore(winner) {
+  if (winner === "human") {
+    humanScore +=1;
+  } else if (winner === "computer") {
+    computerScore +=1;
+  }
+}
+
+function advanceRound() {
+  currentRoundNumber += 1;
+}
+
+compareGuesses(4, generateTarget(), generateTarget());
+updateScore(winner);
+advanceRound();
